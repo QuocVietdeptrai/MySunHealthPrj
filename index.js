ff678efe990f8220b4ed38b0f6d080057afd3528
Quốc Việt Nguyễn <nguyenquocviet2004tb1@gmail.com>
2025-03-25 16:08:10 +0700

    Tich hop Pug

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+// Thiết lập thư mục views 
+app.set('views', path.join(__dirname,"views"));
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('Trang chủ')
+  res.render("client/pages/home",{
+    pageTitle:"Trang chủ1"
+  });
 })
 app.get('/tours', (req, res) => {
-    res.send('Trang danh sách tour')
+    res.render("client/pages/tour_list",{
+        pageTilte:"Danh sách Tour"
+    });
   })
 
 app.listen(port, () => {
