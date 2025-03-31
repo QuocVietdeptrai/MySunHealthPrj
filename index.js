9201fac32cecb0312ca63bd25b50752112862b3f
Quốc Việt Nguyễn <nguyenquocviet2004tb1@gmail.com>
2025-03-31 22:10:22 +0700

    Tich hop sua duong dan cho sinh dong

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const dataBase = require("./config/database");
 const clientRoutes = require("./routes/client/index.route");
 const adminRoutes = require("./routes/admin/index.route");
-
+const variableConfig = require("./config/variable");
 
 const app = express()
 const port = 3000
@@ -19,10 +19,11 @@ app.set('view engine', 'pug')
 // Nhúng file tĩnh muốn public lên 
 app.use(express.static(path.join(__dirname,"public")));
 
-
+// Tạo biến toàn cục trong file PUG 
+app.locals.pathAdmin = variableConfig.pathAdmin;
 //Thiết lập đường dẫn
 app.use("/",clientRoutes);
-app.use("/admin", adminRoutes);
+app.use(`/${variableConfig.pathAdmin}`, adminRoutes);
 
 app.listen(port, () => {
   console.log(`Website đang chạy trên cổng ${port}`)
