e9f9a7039475da10b4a76e41d03be0450ae465ed
Quốc Việt Nguyễn <nguyenquocviet2004tb1@gmail.com>
2025-03-31 22:46:46 +0700

    Tich hop trang tong quan Admin

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const accountRoutes = require("./account.route");
+const dashboardRoutes = require("./dashboard.route");
 
+router.use('/dashboard', dashboardRoutes);
 router.use('/account', accountRoutes);
+
 
 module.exports = router;
