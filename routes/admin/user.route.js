ccbf1fd125c6da1e14fc7b2b74d346975099e902
Quốc Việt Nguyễn <nguyenquocviet2004tb1@gmail.com>
2025-04-04 16:06:33 +0700

    Tich hop cac duong link cho admin

--- routes/admin/user.route.js
+++ routes/admin/user.route.js
@@ -2,8 +2,4 @@ const router = require('express').Router();
 const userController = require("../../controllers/admin/user.controller");
 
 router.get('/list', userController.list);
-
-
-
-
 module.exports = router;
