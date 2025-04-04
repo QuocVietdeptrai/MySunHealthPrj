c3acd865ff1875f059e330d9192e36439fe6ae1e
Quốc Việt Nguyễn <nguyenquocviet2004tb1@gmail.com>
2025-04-04 22:39:21 +0700

    Sua cay lan cuoi

--- validates/admin/account.validate.js
+++ validates/admin/account.validate.js
@@ -1,35 +1,35 @@
-// const Joi = require('joi');
-// module.exports.registerPost = (req, res, next) => { 
-//     const schema = Joi.object({
-//         fullName: Joi.string()
-//             .required()
-//             .message({
-//                 "string.empty" : "Vui lòng nhập họ tên !"
-//             }),
+const Joi = require('joi');
+module.exports.registerPost = (req, res, next) => { 
+    const schema = Joi.object({
+        fullName: Joi.string()
+            .required()
+            .message({
+                "string.empty" : "Vui lòng nhập họ tên !"
+            }),
 
-//         email: Joi.string()
-//             .required()
-//             .message({
-//                 "string.empty" : "Vui lòng nhập email !"
-//             }),
+        email: Joi.string()
+            .required()
+            .message({
+                "string.empty" : "Vui lòng nhập email !"
+            }),
 
-//         password: Joi.string()
-//             .required()
-//             .message({
-//                 "string.empty" : "Vui lòng nhập mật khẩu !"
-//             }),
-//     });
-//     const { error } = schema.validate(req.body);
+        password: Joi.string()
+            .required()
+            .message({
+                "string.empty" : "Vui lòng nhập mật khẩu !"
+            }),
+    });
+    const { error } = schema.validate(req.body);
 
-//     if(error){
-//         console.log(error);
-//         res.json({
-//             code:"error",
-//             message: "Lỗi!"
-//         });
-//         return;
-//     }
+    if(error){
+        console.log(error);
+        res.json({
+            code:"error",
+            message: "Lỗi!"
+        });
+        return;
+    }
 
 
-//     next();
-// }
+    next();
+}
