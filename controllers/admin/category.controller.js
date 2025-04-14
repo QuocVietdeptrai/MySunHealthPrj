f45020246a25d07af4b122c7743730489215a817
Quốc Việt Nguyễn <nguyenquocviet2004tb1@gmail.com>
2025-04-14 16:31:33 +0700

    Tao danh muc o muc do co ban

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -1,10 +1,31 @@
-module.exports.list = (req, res) => {
+const Category = require("../../models/category.model");
+
+module.exports.list =async (req, res) => {
     res.render("admin/pages/category-list",{
       pageTitle:"Quản lý danh mục"
     });
 }
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
   res.render("admin/pages/category-create",{
     pageTitle:"Tạo danh mục"
   });
+}
+module.exports.createPost = async (req, res) => {
+  if(req.body.position){
+    req.body.position = parseInt(req.body.position);
+  }else{
+    const totalRecord = await Category.countDocuments({})
+    req.body.position = totalRecord + 1;
+  }
+
+  req.body.createdBy = req.account.id;
+  req.body.updatedBy = req.account.id;
+
+  const newRecord = new Category(req.body);
+  await newRecord.save();
+  console.log(req.body);
+  res.json({
+    code:"success",
+    message: "Tạo danh mục thành công !"
+  })
 }
