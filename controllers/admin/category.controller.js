65149754ca63ac827f24488e37112a22174c70df
QuocVietdeptrai <nguyenquocviet2004tb12@gmail.com>
2025-04-17 21:38:57 +0700

    Hien thi du lieu trong mongo len trang list danh mục

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -1,9 +1,42 @@
 const Category = require("../../models/category.model");
+const AccountAdmin = require("../../models/account-admin.model")
 const categoryHelper = require("../../helpers/category.helper")
+const moment = require('moment'); 
 
 module.exports.list =async (req, res) => {
+  const categoryList = await Category.find({
+    deleted:false
+  }).sort({
+    position:"desc"
+  })
+  // console.log(categoryList);
+  for (const item of categoryList) {
+    if(item.createdBy){
+      const infoAccountCreatedBy = await AccountAdmin.findOne({
+        _id:item.createdBy
+      })
+      item.createdByFullName =infoAccountCreatedBy.fullName   
+      console.log(infoAccountCreatedBy)
+    }
+
+    if(item.updatedBy){
+      const infoAccountUpdatedBy = await AccountAdmin.findOne({
+        _id:item.createdBy
+      })
+      item.updatedByFullName =infoAccountUpdatedBy.fullName   
+      console.log(infoAccountUpdatedBy)
+    }
+
+    
+    item.createdAtFomat = moment(item.createdAt).format("HH:mm - DD/MM/YYYY");
+    item.updatedAtFomat = moment(item.updatedAt).format("HH:mm - DD/MM/YYYY");
+    
+
+    
+  }
     res.render("admin/pages/category-list",{
-      pageTitle:"Quản lý danh mục"
+      pageTitle:"Quản lý danh mục",
+      categoryList : categoryList
     });
 }
 module.exports.create = async (req, res) => {
@@ -12,7 +45,7 @@ module.exports.create = async (req, res) => {
   })
   const categoryTree = categoryHelper.buildCategoryTree(categoryList);
   
-  console.log(categoryTree)
+  // console.log(categoryTree)
   res.render("admin/pages/category-create",{
     pageTitle:"Tạo danh mục",
     categoryList:categoryTree
