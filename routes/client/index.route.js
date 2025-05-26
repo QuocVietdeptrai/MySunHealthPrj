10809277efffbb5a3291a09d2b8ea9327204e473
QuocVietdeptrai <nguyenquocviet2004tb12@gmail.com>
2025-05-27 00:07:43 +0700

    Tính năng phân quyền

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,7 +3,8 @@ const tourRoutes = require("./tour.route");
 const homeRoutes = require("./home.route");
 const cartRoutes = require("./cart.route");
 
-
+const settingMiddleWares = require("../../middlewares/client/setting.middlewares")
+router.use(settingMiddleWares.websiteInfo);
 router.use('/', homeRoutes);
 router.use('/tours', tourRoutes);
 router.use('/cart', cartRoutes);
