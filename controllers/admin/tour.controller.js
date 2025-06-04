8895848fb48882e4180d729238104de032dee2d1
QuocVietdeptrai <nguyenquocviet2004tb12@gmail.com>
2025-06-05 00:22:09 +0700

    Upload nhiều ảnh ở tour-detail

--- controllers/admin/tour.controller.js
+++ controllers/admin/tour.controller.js
@@ -176,7 +176,11 @@ module.exports.createPost = async (req, res) => {
 
   req.body.createdBy = req.account.id;
   req.body.updatedBy = req.account.id;
-  req.body.avatar = req.file ? req.file.path : "";
+  if(req.files && req.files.avatar) {
+    req.body.avatar = req.files.avatar[0].path;
+  } else {
+    delete req.body.avatar;
+  }
 
   req.body.priceAdult = req.body.priceAdult ? parseInt(req.body.priceAdult) : 0;
   req.body.priceChildren = req.body.priceChildren ? parseInt(req.body.priceChildren) : 0;
@@ -191,6 +195,11 @@ module.exports.createPost = async (req, res) => {
   req.body.departureDate = req.body.departureDate ? new Date(req.body.departureDate) : null;
   req.body.schedules = req.body.locations ? JSON.parse(req.body.schedules) : [];
 
+  if(req.files && req.files.images && req.files.images.length > 0) {
+    req.body.images = req.files.images.map(file => file.path);
+  } else {
+    delete req.body.images;
+  }
   const newRecord = new Tour(req.body);
   await newRecord.save();
 
@@ -323,8 +332,8 @@ module.exports.editPatch = async (req, res) => {
     }
 
     req.body.updatedBy = req.account.id;
-    if(req.file) {
-      req.body.avatar = req.file.path;
+    if(req.files && req.files.avatar) {
+      req.body.avatar = req.files.avatar[0].path;
     } else {
       delete req.body.avatar;
     }
@@ -342,6 +351,11 @@ module.exports.editPatch = async (req, res) => {
     req.body.departureDate = req.body.departureDate ? new Date(req.body.departureDate) : null;
     req.body.schedules = req.body.locations ? JSON.parse(req.body.schedules) : [];
 
+    if(req.files && req.files.images && req.files.images.length > 0) {
+      req.body.images = req.files.images.map(file => file.path);
+    } else {
+      delete req.body.images;
+    }
     await Tour.updateOne({
       _id: id,
       deleted: false
--- routes/admin/tour.route.js
+++ routes/admin/tour.route.js
@@ -8,10 +8,26 @@ const upload = multer({storage: cloudinaryHelper.storage});
 
 router.get('/list', tourController.list);
 router.get('/create', tourController.create);
-router.post('/create',upload.single('avatar'),tourValidate.createPost,tourController.createPost);
+router.post('/create',
+  upload.fields([
+    { name: 'avatar', maxCount: 1 },
+    { name: 'images', maxCount: 10 }
+  ]),
+  tourValidate.createPost,
+  tourController.createPost
+);
 router.get('/trash', tourController.trash);
 router.get('/edit/:id', tourController.edit);
-router.patch('/edit/:id',upload.single('avatar'),tourValidate.createPost,tourController.editPatch);
+router.patch(
+  '/edit/:id', 
+  upload.fields([
+    { name: 'avatar', maxCount: 1 },
+    { name: 'images', maxCount: 10 }
+  ]), 
+  tourValidate.createPost,
+  tourController.editPatch
+)
+
 router.patch('/delete/:id', tourController.deletePatch);
 router.patch('/undo/:id', tourController.undoPatch)
 router.patch('/delete-destroy/:id', tourController.deleteDestroyPatch)
